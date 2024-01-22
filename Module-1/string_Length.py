8b5b81f7ec8d8aab235355e9296d838b0541d873
poojamavadia <156043656+poojamavadia@users.noreply.github.com>
2024-01-22 19:02:09 +0530

    Programs

---
+++ Module-1/string_Length.py
@@ -0,0 +1,6 @@
+#write a python program  to calculate length of a string.
+character_array=input("Enter string:")
+Length=0
+for char in character_array:
+    Length+=1
+print(f"Length of a given string is:{Length}")
