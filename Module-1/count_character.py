615fe96e9c70fcdf86582e172de50fa14b1230f4
poojamavadia <156043656+poojamavadia@users.noreply.github.com>
2024-01-22 19:18:29 +0530

    programs

---
+++ Module-1/count_character.py
@@ -0,0 +1,8 @@
+#write a python program to count number of character in a string.
+character_array=input("Enter string:")
+
+count=0
+for i in character_array:
+    count+=1
+
+print(f"Number of character in a given string...:{count}")
